926be62b756d362bb7bef710bb238f4e4868eced
amithreddy <amithreddy576r@gmail.com>
2020-05-24 10:00:30 +0530

    Editing navbar types of users

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,7 @@ ACCOUNT_TYPES = (
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'profile')
     type = models.CharField(max_length=6, choices=ACCOUNT_TYPES)
 
     def __str__(self):
--- accounts/views.py
+++ accounts/views.py
@@ -13,9 +13,8 @@ def register(request):
         if u_form.is_valid() and p_form.is_valid() :
             u_form.save()
             user = User.objects.get(username=u_form.cleaned_data.get('username'))
-            profile = Profile.objects.create(user= user)
-            profile.type = p_form.cleaned_data.get('type')
-            profile.save()
+            profile = Profile.objects.create(user= user,type=p_form.cleaned_data.get('type'))
+            user.profile.save()
             if profile.type in ['PATIENT','patient']:
                 patient = PatientBio.objects.create(patient =user)
                 patient.save()
@@ -35,4 +34,5 @@ def register(request):
 
 
 def index(request):
+    # print(Profile.objects.get(user = request.user).type)
     return render(request,'accounts/base.html')
